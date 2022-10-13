3477baf07e5db2e768803fbc2c79139ae0d5b703
dharmajitb <dharmajit.budha@machnetinc.com>
2022-10-13 22:12:44 +0545

    Implement JWT and Generat user authenticate Token

--- eCommerce/src/main/java/com/demo/ecommerce/config/SecurityConfig.java
+++ eCommerce/src/main/java/com/demo/ecommerce/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package com.demo.ecommerce.config;
 
 
 import com.demo.ecommerce.service.Implement.AuthorServiceImplement;
+import com.demo.ecommerce.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -17,12 +21,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthorServiceImplement authorServiceImplement;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(authorServiceImplement);
     }
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
+                .permitAll().anyRequest().authenticated();
     }
 }
--- eCommerce/src/main/java/com/demo/ecommerce/controller/UserController.java
+++ eCommerce/src/main/java/com/demo/ecommerce/controller/UserController.java
@@ -2,8 +2,11 @@ package com.demo.ecommerce.controller;
 
 import com.demo.ecommerce.model.Author;
 import com.demo.ecommerce.service.AuthorService;
+import com.demo.ecommerce.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,6 +14,12 @@ public class UserController {
 
     @Autowired
     private AuthorService authorService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @PostMapping("/")
     public ResponseEntity<Author> addProduct(@RequestBody Author author) throws Exception {
@@ -23,6 +32,16 @@ public class UserController {
         return this.authorService.getUser(username);
     }
 
+    @PostMapping("/authenticate")
+    public String generateToken(@RequestBody Author author) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(author.getUsername(),author.getPassword()));
+        }catch (Exception ex){
+            throw new Exception("Invalid username or password");
+        }
+        return jwtUtil.generateToken(author.getUsername());
+
+    }
 
     @GetMapping("/")
     public String welcome() {
