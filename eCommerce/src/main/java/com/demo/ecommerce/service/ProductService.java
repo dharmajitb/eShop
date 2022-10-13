bb974b767a9e83bfdfef50c61638b7e98356fff7
dharmajitb <dharmajit.budha@machnetinc.com>
2022-10-13 19:50:43 +0545

    remove unwanted lines

--- eCommerce/src/main/java/com/demo/ecommerce/service/ProductService.java
+++ eCommerce/src/main/java/com/demo/ecommerce/service/ProductService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Set;
 
-@Service
+
 public interface ProductService {
 
     public Product addProduct(Product product);
@@ -17,26 +17,6 @@ public interface ProductService {
     public Product getProduct(Long id);
     public void deleteProduct(long id);
 
-//    @Autowired
-//    private ProductRepository productRepository;
 
-//    //add
-//    public void addProduct(Product product){
-//        productRepository.save(product);
-//    }
-//    //delete
-//    public void deleteProduct (long id){
-//        productRepository.deleteById(id);
-//    }
-//
-//    //get
-//    public Product getProductById(long id){
-//        return productRepository.getOne(id);
-//    }
-//
-//    public List<Product> getAllProduct(){
-//        return productRepository.findAll();
-//    }
-//
 
 }
